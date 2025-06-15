a79f845b8c17451ffe74a714934b28e55ca8adce
Amidza-Idris <hidris2003@gmail.com>
2025-06-15 14:51:27 +0200

    Backend Completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,31 @@
-import express from 'express';
+// import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+// import messageRoutes from "./routes/messageRoutes.js";
+// import { v2 as cloudinary } from "cloudinary";
+// import { app, server } from "./socket/socket.js";
+// import job from "./cron/cron.js";
 
+dotenv.config();
+connectDB();
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use(cookieParser()); // Middleware to parse cookies
+
+// Routes
+app.use("/api/users", userRoutes); // User-related routes
+app.use("/api/posts", postRoutes); // Post-related routes
+// app.use("/api/messages", messageRoutes); // Message-related routes
+
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-}
-);
+  console.log("Server is running on port 5000");
+});
