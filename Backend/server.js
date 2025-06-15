ebabb68d3225db64796db6a5d9a6cea23e10b271
Amidza-Idris <hidris2003@gmail.com>
2025-06-15 15:25:09 +0200

    Frontend changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
@@ -15,7 +15,16 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
+// Cloudinary configuration not ready yet
+/*
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+*/
 // Middleware
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
@@ -26,6 +35,22 @@ app.use("/api/users", userRoutes); // User-related routes
 app.use("/api/posts", postRoutes); // Post-related routes
 // app.use("/api/messages", messageRoutes); // Message-related routes
 
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+/*
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // react app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
+*/
